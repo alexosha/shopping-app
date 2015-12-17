5b0386f3e27418ff43a97610f58d0c81644b0e5a
Alexander Osha <alexosha@gmail.com>
2015-12-16 21:20:48 -0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@ $(document).ready(function(){
 	$("button.add").click(function() {
     	var new_item = $("input.add-item").val();
     	$( "ul.list" ).append("<li>"+new_item+"</li>");
+    	$( "select.remove-item" ).append("<option>"+new_item+"</option>");
     	return false;
     
      });
 	$('ul').on('click', 'li', function(event){
   $(this).toggleClass("strike-through");
 });
+	$("option.remove-existing").click(function(){
+
+	});
 });
 // <label><input type="checkbox" value="None" id="squaredTwo" name="check" />
 //</label>
